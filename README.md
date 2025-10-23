281035e399bef0484a745cec948c1e1d27b37587
Katie McLaughlin <katie@glasnt.com>
2025-10-23 14:29:39 +1100

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cotton Eye
+
+➡️[Go to deployed service](https://deploy-1068624597505.us-central1.run.app)⬅️
+
+A service that uses referer headers to show how to deploy said service, if you linked on the above link from the GitHub repo page.
---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+
+from urllib.parse import urlparse
+
+import github
+
+
+
+app = Flask(__name__)
+
+g = github.Github()
+
+def get_context(github_url):
+    """From a GitHub URL, return the elements required to use it in git"""
+    url = urlparse(github_url)
+
+    path_segments = list(filter(None, url.path.split("/")))
+    repo_slug = "/".join(path_segments[0:2])
+
+    repo = g.get_repo(repo_slug)
+
+    if len(path_segments) > 4:
+        branch = "/".join(path_segments[3:4])
+        directory = "/".join(path_segments[4:])
+    else:
+        branch = repo.default_branch
+        directory = "."
+
+    context = {"repo": repo_slug, "branch": branch, "dir": directory}
+    return context
+
+
+@app.get("/")
+def home():
+    context = {}
+
+    if "referer" in request.headers:
+        if "https://github.com" in request.headers["referer"]:
+            context = get_context(request.headers["referer"])
+
+    return render_template("index.html", **context)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
